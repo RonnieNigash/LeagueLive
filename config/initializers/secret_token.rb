9fc6179bb0e296d1092a614f8bbc85025173b4fc
Ian Mitchell <ian.mitchel1@live.com>
2013-01-17 18:33:28 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeagueLive::Application.config.secret_token = '0cbe04f8477be33a1d9f617c97a11378aa1faa7898ece3b61aabc666edc2ef839355819533eba08b1757e04b9f7cbd98751453c1ee22db1256a0c228c6c5f5c6'
