81baac79860fecffe45b3f665492fcd1ac42fafa
Ian Mitchell <ian.mitchel1@live.com>
2013-01-22 10:39:40 -0800

    Added username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  validates :name,    :presence => true,
+                      :uniqueness => true
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
