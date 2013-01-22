d27d8716c343f5ef3b986d9ce91e7be052ae9880
Ian Mitchell <ian.mitchel1@live.com>
2013-01-22 14:00:01 -0800

    Added Login / Register homepage forms
    Temporary Redirect on the Homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,10 @@ class HomepageController < ApplicationController
   layout "desktop"
 
   def index
+    if user_signed_in?
+      # TODO: Change this to Admin Panel URL
+      redirect_to about_url
+    end
   end
 
   def about
