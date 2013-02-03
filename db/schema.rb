e0979b45fc6bc630233eb70a33ddbcd7e5ff1229
Ian Mitchell <ian.mitchel1@live.com>
2013-02-03 14:31:31 -0800

    Added Promoted Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130203221757) do
+ActiveRecord::Schema.define(:version => 20130203223023) do
 
   create_table "champions", :force => true do |t|
     t.string   "name"
@@ -122,8 +122,8 @@ ActiveRecord::Schema.define(:version => 20130203221757) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -132,10 +132,11 @@ ActiveRecord::Schema.define(:version => 20130203221757) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
     t.string   "slug"
+    t.boolean  "promoted",               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
