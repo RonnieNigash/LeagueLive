2f76e3ece059c7b48672ca43b3900ec81f6b0347
Ian Mitchell <ian.mitchel1@live.com>
2013-02-04 23:08:38 -0800

    Added Bans to Stats Grid

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,8 +4,8 @@ class Game < ActiveRecord::Base
   belongs_to :purple_team, :class_name => "Team"
   belongs_to :blue_team, :class_name => "Team"
 
-  has_one :blue_stats, :class_name => "Stats"
-  has_one :purple_stats, :class_name => "Stats"
+  has_one :blue_stats, :class_name => "Stats", :foreign_key => "id", :primary_key => "blue_stats_id"
+  has_one :purple_stats, :class_name => "Stats", :foreign_key => "id", :primary_key => "purple_stats_id"
 
   has_many :commentaries
   has_many :player_stats
--- app/models/stats.rb
+++ app/models/stats.rb
@@ -1,9 +1,9 @@
 class Stats < ActiveRecord::Base
   belongs_to :game
+  belongs_to :first_ban, :class_name => "Champion"
+  belongs_to :second_ban, :class_name => "Champion"
+  belongs_to :third_ban, :class_name => "Champion"
 
-  has_one :first_ban, :class_name => "Champion"
-  has_one :second_ban, :class_name => "Champion"
-  has_one :third_ban, :class_name => "Champion"
 
   attr_accessible :first_ban, :gold, :inhibitors, :second_ban, :third_ban, :towers
 
